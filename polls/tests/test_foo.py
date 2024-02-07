3056e7069fab7ca06515550b6cfbf7456917405d
Justin Hickman <justin@vertispan.com>
2024-02-07 14:27:11 -0500

    Added a test that doesn't require database

--- polls/tests/test_foo.py
+++ polls/tests/test_foo.py
@@ -9,3 +9,10 @@ class TestSomething:
     def test_id(self):
         questions = Question.objects.all()
         assert len(questions) == 1
+
+
+
+class TestSomethingWithoutDatabase:
+    def test_something(self):
+        assert 1 == 1
+        
